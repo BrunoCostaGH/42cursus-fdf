429e4210b8fbb9c2ae036a31660474a7f1e5430d
Bruno Costa <54143084+BrunoCostaGH@users.noreply.github.com>
2023-03-03 19:44:52 +0000

    feat(*): *

---
+++ main.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsilva-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/08 13:48:36 by bsilva-c          #+#    #+#             */
+/*   Updated: 2023/02/16 15:12:59 by bsilva-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+	Rendering
+ Your program has to represent the model in isometric projection.
+ The coordinates of the landscape are stored in a .fdf file passed as a parameter
+  to your program. Here is an example:
+	$>cat 42.fdf
+		0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
+		0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
+		0  0  10 10 0  0  10 10 0  0  0  10 10 10 10 10 10 0  0
+		0  0  10 10 0  0  10 10 0  0  0  10 10 10 10 10 10 0  0
+		0  0  10 10 0  0  10 10 0  0  0  0  0  0  0  10 10 0  0
+		0  0  10 10 10 10 10 10 0  0  0  10 10 10 10 10 10 0  0
+		0  0  10 10 10 10 10 10 0  0  0  10 10 10 10 10 10 0  0
+		0  0  0  0  0  0  10 10 0  0  0  10 10 0  0  0  0  0  0
+		0  0  0  0  0  0  10 10 0  0  0  10 10 10 10 10 10 0  0
+		0  0  0  0  0  0  10 10 0  0  0  10 10 10 10 10 10 0  0
+		0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
+
+	Each number represents a point in space:
+		• The horizontal position corresponds to its axis.
+		• The vertical position corresponds to its ordinate.
+		• The value corresponds to its altitude.
+	Keep in mind that the goal of this project is not to parse maps! However,
+	 this doesn’t mean that your program should crash when run. It means that
+	  we assume the map contained in the file is properly formatted.
+
+	Graphic management
+• Your program has to display the image in a win.
+• The management of your win must remain smooth (changing to another win, 
+minimizing, and so forth).
+• Pressing ESC must close the win and quit the program in a clean way.
+• Clicking on the cross on the win’s frame must close the win and quit the 
+program in a clean way.
+• The use of the images of the MiniLibX is mandatory.
+*/
+
+#include "fdf.h"
+
+static int	close_win(int keycode, void *param)
+{
+	if (keycode == 65307 || (int *)param == (int *)347)
+		exit(0);
+	return (0);
+}
+
+// Create window and set hooks for 'x' close and ESC button exit, respectively.
+static void	create_window(t_mlx *mlx)
+{
+	t_mlx_win	*win;
+
+	win = mlx->win;
+	win->id = mlx_new_window(mlx->id, WIN_X, WIN_Y, "FDF by bsilva-c");
+	mlx_hook(win->id, 17, 1L << 17, close_win, NULL);
+	mlx_hook(win->id, 2, 1L << 0, close_win, NULL);
+}
+
+//	Clear MLX struct.
+static void	clear_mlx(t_mlx	*mlx)
+{
+	mlx_clear_window(mlx->id, mlx->win->id);
+	mlx_destroy_window(mlx->id, mlx->win->id);
+	free_mlx(mlx);
+}
+
+//	Check if files exists.
+int	check_file(char *argv)
+{
+	int	fd;
+	int	status;
+
+	fd = open(argv, O_RDONLY);
+	status = 0;
+	if (read(fd, 0, 0) == -1)
+		status = 1;
+	close(fd);
+	return (status);
+}
+
+/*
+	1. Check if file exists.
+	2. Read Map from file.
+	3. Generate MLX Window.
+	4. Generate Image with drawing.
+	5. MLX_LOOP to keep window open and program running.
+*/
+int	main(int argc, char **argv)
+{
+	char		*file_ext;
+	t_mlx		*mlx;
+
+	if (argc == 2)
+	{
+		file_ext = ft_strchr(argv[1], '.');
+		if (!ft_strncmp(file_ext, ".fdf", ft_strlen(file_ext)) \
+			&& check_file(argv[1]) == 0)
+		{
+			mlx = generate_t_mlx(argv[1]);
+			read_map(argv[1], mlx);
+			mlx->id = mlx_init();
+			create_window(mlx);
+			create_image(mlx, argv[1]);
+			mlx_loop(mlx->id);
+			clear_mlx(mlx);
+		}
+	}
+	return (0);
+}
---
+++ win_n_draw.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   win_n_draw.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsilva-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/20 09:12:46 by bsilva-c          #+#    #+#             */
+/*   Updated: 2023/01/18 13:51:33 by bsilva-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+//Draw pixel.
+void	pix(t_mlx *mlx, int x, int y, int color)
+{
+	char		*dst;
+	t_mlx_img	*img;
+
+	img = mlx->img;
+	dst = img->buffer + (y * img->size_line + x * (img->bpp / 8));
+	*(unsigned int *)dst = color;
+	if (DEV_MODE)
+		mlx_pixel_put(mlx->id, mlx->win->id, x, y, color);
+}
+
+void	draw_line(t_mlx *mlx, t_point *p0, t_point *p1)
+{
+	double	s;
+	t_point	*cur;
+
+	cur = malloc(sizeof(t_point));
+	if (!cur)
+		exit(1);
+	s = (float)(p1->y - p0->y) / (float)(p1->x - p0->x);
+	if (s <= 1 && s >= -1)
+		draw_line_r(mlx, p0, p1, cur);
+	else
+		draw_line_rr(mlx, p0, p1, cur);
+	free(cur);
+}
+
+// calculate final coordinates
+t_point	*set_coords(t_mlx *mlx, char *argv, int y0, int x0)
+{
+	int			x;
+	int			y;
+	int			z;
+	t_point		*margin;
+	t_point		*point;
+
+	margin = malloc(sizeof(t_point));
+	point = malloc(sizeof(t_point));
+	if (!point || !margin)
+		exit (1);
+	margin->y = count_y_cols(argv, 1);
+	margin->x = count_x_rows(argv);
+	while ((y0 * mlx->zoom) > (WIN_X / 2) || (x0 * mlx->zoom) > (WIN_Y / 2))
+		mlx->zoom--;
+	x = x0 * mlx->zoom;
+	y = y0 * mlx->zoom;
+	z = mlx->map[y0]->alt[x0]->value;
+	point->x = ((x - y) * cos(0.523599) + margin->x * mlx->zoom + mlx->zoom);
+	point->y = (-z + (x + y) * sin(0.523599) + margin->y * mlx->zoom / 2);
+	point->z = z;
+	point->color = mlx->map[y0]->alt[x0]->color;
+	return (point);
+}
+
+// Initiate map draw.
+void	draw_map(t_mlx	*mlx, char *argv)
+{
+	int		x;
+	int		y;
+	int		zoom;
+
+	y = 0;
+	zoom = mlx->zoom;
+	while (mlx->map[y])
+	{
+		x = 0;
+		if (zoom != mlx->zoom)
+		{
+			mlx_destroy_image(mlx->id, mlx->img->id);
+			create_image(mlx, argv);
+			return ;
+		}
+		if (draw_map_x(mlx, argv, y, x) == 1)
+			return ;
+		y++;
+	}
+}
+
+// Create image and draw map.
+void	create_image(t_mlx *mlx, char *argv)
+{
+	t_mlx_img	*img;
+
+	img = mlx->img;
+	img->id = mlx_new_image(mlx->id, WIN_X, WIN_Y);
+	img->buffer = mlx_get_data_addr(img->id, &img->bpp, \
+		&img->size_line, &img->endian);
+	draw_map(mlx, argv);
+	mlx_put_image_to_window(mlx->id, mlx->win->id, img->id, 0, 0);
+}
